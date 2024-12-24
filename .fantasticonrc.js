b6561f1c65e9ea10799d8ff1d81522936f4ddef0
Graham Hibbert <graham@eyefood.co.uk>
2024-12-24 20:55:37 +0000

    Set up storybook url and linting. fonts to /fonts svgs in assets

--- .fantasticonrc.js
+++ .fantasticonrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-	inputDir: './src/icons',
-	outputDir: './src/components/icons',
-	fontTypes: ['ttf', 'woff', 'woff2'],
-	assetTypes: ['ts', 'json', 'css'],
-  };
+	inputDir: "./src/assets/icons",
+	outputDir: "./src/components/icons",
+	fontTypes: ["ttf", "woff", "woff2"],
+	assetTypes: ["ts", "json", "css"],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/global.scss";`,
-      },
-    },
-  },
-})
+	plugins: [vue()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "./src/styles/global.scss";`,
+			},
+		},
+	},
+});
